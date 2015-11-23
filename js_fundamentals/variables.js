4469871bf2eced41c134462acdc78b65bc58d47f
stephthi <nstephanie2@gmail.com>
2015-11-23 14:48:03 -0500

    Completed variables.js

--- js_fundamentals/variables.js
+++ js_fundamentals/variables.js
@@ -1,46 +1,37 @@
-// Exercises: Variables
-
-// EXERCISE: The Fortune Teller
-
-// Why pay a fortune teller when you can just program your fortune yourself?
-
-// Store the following into variables: number of children, partner's name, geographic location, job title.
-// Output your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids."
-
-// EXERCISE: The Age Calculator
-
-// Forgot how old someone is? Calculate it!
-
-// Store the current year in a variable.
-// Store their birth year in a variable.
-// Calculate their 2 possible ages based on the stored values.
-// Output them to the screen like so: "They are either NN or NN", substituting the values.
-
-// EXERCISE: The Lifetime Supply Calculator
-
-// Ever wonder how much a "lifetime supply" of your favorite snack is? Wonder no more!
-
-// Store your current age into a variable.
-// Store a maximum age into a variable.
-// Store an estimated amount per day (as a number).
-// Calculate how many you would eat total for the rest of your life.
-// Output the result to the screen like so: "You will need NN to last you until the ripe old age of X".
-
-// EXERCISE: The Geometrizer
-
-// Calculate properties of a circle, using the definitions here.
-
-// Store a radius into a variable.
-// Calculate the circumference based on the radius, and output "The circumference is NN".
-// Calculate the area based on the radius, and output "The area is NN".
-
-// EXERCISE: The Temperature Converter
-
-// It's hot out! Let's make a converter based on the steps here.
-
-// Store a celsius temperature into a variable.
-// Convert it to fahrenheit and output "NN°C is NN°F".
-// Now store a fahrenheit temperature into a variable.
-// Convert it to celsius and output "NN°F is NN°C."
+function fortuneTeller(children, partner, location, job) {
+	var output = "You will be a " + job + " in " + location + ", and married to " + partner + " with " + children + " kids." ;
+	return output;
+}
 
 
+function ageCalculator(birthYear, currentYear) {
+	var age = currentYear - birthYear;
+	var otherAge = currentYear - birthYear - 1;
+	var output = "They are either " + age + " or " + otherage + "." ;
+	return output;
+}
+
+function supplyCalculator(currentAge, maxAge, amount) {
+	var lifespan = maxAge - currentAge;
+	var foodAmount = lifespan * amount;
+	var output = "You will need " + foodAmount + " to last you until the ripe old age of " + lifespan + "." ;
+	return output;
+}
+
+function geometrizer(radius) {
+	var circumference = 2 * radius * 3.14;
+	var area = radius ^ 2 * 3.14;
+	var output = "The circumference is " + circumference + ". The area is " + area ;
+	return output;
+}
+
+function tempCalculator(celcius) {
+	var fahrenheit = celcius * 1.8 + 32;
+	var output = celcius + "°C is " + fahrenheit + "°F.";
+	return output;
+
+function celciusCalc(fahrenheit)
+	var celcius = (fahrenheit - 32) / 1.8;
+	var output = fahrenheit + "°F is " + celcius + "°C.";
+	return output;
+}
