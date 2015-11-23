6b75bf0cc8b88c241cb1d2670ea0586afe71df8e
stephthi <nstephanie2@gmail.com>
2015-11-23 14:53:03 -0500

    Completed functions.js

--- js_fundamentals/functions.js
+++ js_fundamentals/functions.js
@@ -1,59 +1,37 @@
-// Exercises: Functions
+function fortuneTeller(children, partner, location, job) {
+	var output = "You will be a " + job + " in " + location + ", and married to " + partner + " with " + children + " kids." ;
+	return output;
+}
 
-// EXERCISE: The Fortune Teller
 
-// Why pay a fortune teller when you can just program your fortune yourself?
+function ageCalculator(birthYear, currentYear) {
+	var age = currentYear - birthYear;
+	var otherAge = currentYear - birthYear - 1;
+	var output = "They are either " + age + " or " + otherage + "." ;
+	return output;
+}
 
-// Write a function named tellFortune that:
-// takes 4 arguments: number of children, partner's name, geographic location, job title.
-// outputs your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids."
-// Call that function 3 times with 3 different values for the arguments.
+function supplyCalculator(currentAge, maxAge, amount) {
+	var lifespan = maxAge - currentAge;
+	var foodAmount = lifespan * amount;
+	var output = "You will need " + foodAmount + " to last you until the ripe old age of " + lifespan + "." ;
+	return output;
+}
 
-// EXERCISE: The Age Calculator
+function geometrizer(radius) {
+	var circumference = 2 * radius * 3.14;
+	var area = radius ^ 2 * 3.14;
+	var output = "The circumference is " + circumference + ". The area is " + area ;
+	return output;
+}
 
-// Forgot how old you are? Calculate it!
+function tempCalculator(celcius) {
+	var fahrenheit = celcius * 1.8 + 32;
+	var output = celcius + "°C is " + fahrenheit + "°F.";
+	return output;
 
-// Write a function named calculateAge that:
-// takes 2 arguments: birth year, current year.
-// calculates the 2 possible ages based on those years.
-// outputs the result to the screen like so: "You are either NN or NN"
-// Call the function three times with different sets of values.
-// Bonus: Figure out how to get the current year in JavaScript instead of passing it in.
-
-// EXERCISE: The Lifetime Supply Calculator
-
-// Ever wonder how much a "lifetime supply" of your favorite snack is? Wonder no more!
-
-// Write a function named calculateSupply that:
-// takes 2 arguments: age, amount per day.
-// calculates the amount consumed for rest of the life (based on a constant max age).
-// outputs the result to the screen like so: "You will need NN to last you until the ripe old age of X"
-// Call that function three times, passing in different values each time.
-// Bonus: Accept floating point values for amount per day, and round the result to a round number.
-
-// EXERCISE: The Geometrizer
-
-// Create 2 functions that calculate properties of a circle, using the definitions here.
-
-// Create a function called calcCircumfrence:
-
-// Pass the radius to the function.
-// Calculate the circumference based on the radius, and output "The circumference is NN".
-// Create a function called calcArea:
-
-// Pass the radius to the function.
-// Calculate the area based on the radius, and output "The area is NN".
-
-// EXERCISE: The Temperature Converter
-
-// It's hot out! Let's make a converter based on the steps here.
-
-// Create a function called celsiusToFahrenheit:
-
-// Store a celsius temperature into a variable.
-// Convert it to fahrenheit and output "NN°C is NN°F".
-// Create a function called fahrenheitToCelsius:
-
-// Now store a fahrenheit temperature into a variable.
-// Convert it to celsius and output "NN°F is NN°C."
-
+function celciusCalc(fahrenheit)
+	var celcius = (fahrenheit - 32) / 1.8;
+	var output = fahrenheit + "°F is " + celcius + "°C.";
+	return output;
+}
