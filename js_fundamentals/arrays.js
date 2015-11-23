67b4588496d888e1ff441cb74bdb959c6677bcf0
stephthi <nstephanie2@gmail.com>
2015-11-23 15:43:04 -0500

    Completed arrays.js

--- js_fundamentals/arrays.js
+++ js_fundamentals/arrays.js
@@ -1,10 +1,5 @@
-// Exercises: Arrays
+var favourites = ["green", "red", "blue"]
 
-// EXERCISE: Your top choices
-
-// Create an array to hold your top choices (colors, presidents, whatever).
-// For each choice, log to the screen a string like: "My #1 choice is blue."
-// Bonus: Change it to log "My 1st choice, "My 2nd choice", "My 3rd choice", picking the right suffix for the number based on what it is.
-
-
-
+for(var i = 0; i < favourites.length; i++) {
+  console.log("My #" + (i+1) + " choice is " + my_array[i]);
+}
